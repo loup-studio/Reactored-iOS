eac8e84b5fa6ce4c09512dd99767e64987bfee2b
Lukasz <lukasz@intheloup.io>
2017-08-31 15:21:28 -0400

    Add reactive extension.

--- Reactored/Classes/ReactiveExtensions.swift
+++ Reactored/Classes/ReactiveExtensions.swift
@@ -14,3 +14,26 @@ extension Disposable {
         bag.put(disposable: self, key: key)
     }
 }
+
+extension ObservableType {
+    
+    public func commit<S: ReactorState>(into store: ReactorStore<S>, silently: Bool = false, reducer: @escaping (inout S, E) -> ()) -> Disposable {
+        return subscribe(onNext: { element in
+            store.commit(silently: silently) { state in
+                reducer(&state, element)
+            }
+        })
+    }
+}
+
+extension PrimitiveSequenceType where TraitType == SingleTrait {
+    
+    public func commit<S: ReactorState>(into store: ReactorStore<S>, silently: Bool = false, reducer: @escaping (inout S, ElementType) -> ()) -> Disposable {
+        
+        return subscribe(onSuccess: { element in
+            store.commit(silently: silently) { state in
+                reducer(&state, element)
+            }
+        })
+    }
+}
